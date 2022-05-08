313c6b1656d03fe8a88334f76871bef2f8792dce
Alper Kinali <alperkinali00@hotmail.com>
2022-05-08 17:03:51 +0300

    chnaged

--- src/NotOrtHesaplama/Main.java
+++ src/NotOrtHesaplama/Main.java
@@ -2,9 +2,6 @@ package NotOrtHesaplama;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
-        System.out.println("dasdsadsadsadsadsad");
-        System.out.println("dasdsadsadsdsdasdasadsadsad");
-      
+        
     }
 }
