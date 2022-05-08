6e580841e92a06adaecf7752474ac30c22cc2f2b
Alper Kinali <alperkinali00@hotmail.com>
2022-05-08 16:42:33 +0300

    changed

--- src/NotOrtHesaplama/Main.java
+++ src/NotOrtHesaplama/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Test");
         System.out.println("dasdsadsadsadsadsad");
+        System.out.println("dasdsadsadsdsdasdasadsadsad");
     }
 }
