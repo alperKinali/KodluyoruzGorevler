42245cf9b445d1b3b3f1c80f31246ad1ce0fd25b
Alper Kinali <alperkinali00@hotmail.com>
2022-05-08 17:09:01 +0300

    changed

---
+++ src/KDVTutarıHesaplayanKod/KDV.java
@@ -0,0 +1,7 @@
+package KDVTutarıHesaplayanKod;
+
+public class KDV {
+    public static void main(String[] args) {
+        System.out.println("Kdv Hesabı");
+    }
+}
--- src/NotOrtHesaplama/Main.java
+++ src/NotOrtHesaplama/Main.java
@@ -3,5 +3,7 @@ package NotOrtHesaplama;
 public class Main {
     public static void main(String[] args) {
         System.out.println("deneme son hal ");
+
+        System.out.println("deneme son hal ");
     }
 }
