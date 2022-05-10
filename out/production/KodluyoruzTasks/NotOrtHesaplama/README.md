63dc48568bac919f4897c2c73fb9584bb96c903c
Alper Kinali <alperkinali00@hotmail.com>
2022-05-10 12:57:06 +0300

    Commited

---
+++ out/production/KodluyoruzTasks/NotOrtHesaplama/README.md
@@ -0,0 +1,4 @@
+
+Not Ortalamasını Hesaplayan Program
+[Patika](https://app.patika.dev/Alperkinali)
+
--- src/NotOrtHesaplama/Main.java
+++ src/NotOrtHesaplama/Main.java
@@ -4,6 +4,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("deneme son hal ");
 
-        System.out.println("deneme son hal ");
     }
 }
