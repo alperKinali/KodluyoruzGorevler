871d4b0ede60332aaa616c3133aacbd49e402919
Alper Kinali <alperkinali00@hotmail.com>
2022-05-11 22:55:42 +0300

    kdv examp commit

--- src/KDVTutarıHesaplayanKod/KDV.java
+++ src/KDVTutarıHesaplayanKod/KDV.java
@@ -1,7 +1,26 @@
 package KDVTutarıHesaplayanKod;
 
+import java.util.Scanner;
+
 public class KDV {
     public static void main(String[] args) {
         System.out.println("Kdv Hesabı");
+        double  tutar, kdv_orn1=0.18, kdv_orn2=0.8;
+        System.out.println("KDV hesaplanacak tutarı giriniz");
+        Scanner scanner = new Scanner(System.in);
+        tutar=scanner.nextDouble();
+        if (tutar>0&& tutar<=1000){
+            System.out.println("KDV Tutarı :"+(tutar*kdv_orn1));
+            System.out.println("KDV'siz fiyat : "+tutar);
+             double y_tutar=(tutar*kdv_orn1)+tutar;
+            System.out.println("Kdv'li fiyat : "+y_tutar);
+        }
+        else if (tutar>1000){
+            System.out.println("KDV Tutarı :"+(tutar*kdv_orn2));
+            System.out.println("KDV'siz fiyat : "+tutar);
+            double y_tutar=(tutar*kdv_orn2)+tutar;
+            System.out.println("Kdv'li fiyat : "+y_tutar);
+        }
+        System.out.println("Program bitti");
     }
 }
